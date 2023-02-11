99c101745f3fd4813a6416623b14a5b976fa8692
kkito0726 <k.kito0726@gmail.com>
2023-02-11 23:11:37 +0900

    コメントを取得し、表示させた

--- frontend/src/components/videoInfo/VideoInfo.jsx
+++ frontend/src/components/videoInfo/VideoInfo.jsx
@@ -1,20 +1,29 @@
 import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { format } from "timeago.js";
+import ThumbUpOffAltIcon from "@mui/icons-material/ThumbUpOffAlt";
 import "./VideoInfo.css";
 
 export const VideoInfo = (props) => {
   const params = useParams();
   const videoId = params.video_id;
   const [data, setData] = useState({});
+  const [commentData, setCommentData] = useState({});
 
   useEffect(() => {
     get_videoInfo();
+    getComments();
   }, []);
 
   const get_videoInfo = async () => {
     const data = await (await axios(`${props.URL}?q=${videoId}`)).data;
     setData(data);
+  };
+
+  const getComments = async () => {
+    const data = await (await axios(`/comment?q=${videoId}`)).data;
+    setCommentData(data);
   };
   return (
     <div className="main">
@@ -36,6 +45,36 @@ export const VideoInfo = (props) => {
       ) : (
         <></>
       )}
+      <div className="commentContainer">
+        <div className="commentWrapper">
+          <span className="commentTitle">コメント</span>
+          <hr className="commentHr" />
+          {commentData.comments ? (
+            commentData.comments.map((comment, idx) => (
+              <div className="comment">
+                <div className="commentUser">
+                  <span className="authorName">{commentData.authors[idx]}</span>
+                  <span className="commentPublishedAt">
+                    {format(commentData.publishedAt[idx])}
+                  </span>
+                </div>
+
+                <div className="commentMain">
+                  <span className="commentDisplay">{comment}</span>
+                </div>
+
+                <div className="commentBottom">
+                  <ThumbUpOffAltIcon />
+                  <span className="likes">{commentData.likeCounts[idx]}</span>
+                </div>
+                <hr className="commentHr" />
+              </div>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
+      </div>
     </div>
   );
 };
